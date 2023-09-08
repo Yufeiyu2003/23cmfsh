dc469f73bfce8e63c91300a75b914153fb98f9b7
Xujinh <xujinh-1@foxmail.com>
2023-09-08 23:20:31 +0800

    a

--- 求镜面法向量.py
+++ 求镜面法向量.py
@@ -9,7 +9,10 @@ def get_mirror_out_vector( mirror_center_point):
     out=HEATING_TOWER_CENTER-mirror_center_point
     return out/ np.linalg.norm(out)
 
-def get_mirror_normal_vector( mirror_center_point, sun_vector):
+def get_mirror_normal_vector( mirror_center_point, sun_vector,out_vector):
+    """
+    out_vector由get_mirror_out_vector函数提供
+    """
     #异常处理
     if(len(mirror_center_point)!=3 or len(sun_vector)!=3):
         print("输入参数错误")
@@ -19,9 +22,9 @@ def get_mirror_normal_vector( mirror_center_point, sun_vector):
     #出射光即为镜面中心与加热塔中心的连线
     #法向量为入射光与出射光方向的中间向量
     #出射光方向
-    out_victor = get_mirror_out_vector(mirror_center_point)
+    #out_vector = get_mirror_out_vector(mirror_center_point)
     #法向量
-    normal_vector = sun_vector + out_victor
+    normal_vector = sun_vector + out_vector
     normal_vector = normal_vector/np.linalg.norm(normal_vector)
     if(normal_vector[2]<0):
         normal_vector = -normal_vector
