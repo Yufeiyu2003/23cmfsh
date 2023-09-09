c0be1d34bd4804f5a53c8b194934e52c697a15eb
Xujinh <xujinh-1@foxmail.com>
2023-09-09 16:56:12 +0800

    修改函数

--- ColumnProjection.py
+++ ColumnProjection.py
@@ -7,7 +7,7 @@ import numpy as np
 from IntersectArea import *
 
 TC=np.array([0,0,80])
-TOWER = np.array([3.5,-4],[3.5,4],[-3.5,4],[-3.5,-4])
+TOWER = np.array([[3.5,-4],[3.5,4],[-3.5,4],[-3.5,-4]])
 
 def ColumnProjection(mirror_to_column:np.array,mirror_points,mirror_center):
     # 通过向量求投射平面
@@ -20,7 +20,6 @@ def ColumnProjection(mirror_to_column:np.array,mirror_points,mirror_center):
     # b= mirror_to_column[1]
     # # plane = ax + by = 0
 
-    ans = []
     for i in range(4):
         # 求距离
 
@@ -46,30 +45,28 @@ def ColumnProjection(mirror_to_column:np.array,mirror_points,mirror_center):
         [0,0,1]]
     )# 旋转矩阵
     points = mirror_points.dot(R)[:,[0,2]]
-    points -= np.array([[0,TC[2]],[0,TC[2]]])
+    points -= np.array([[0,TC[2]],[0,TC[2]],[0,TC[2]],[0,TC[2]]])
     
     # 计算放大倍率
-    p1 = points[0]
-    p2 = points[1]
-    L = np.abs(p1[0]*p2[1]-p1[1]*p2[0])/np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
-    o_amplify_rate = (ex_range+L)/L
-    o_points = points*o_amplify_rate
-    i_amplify_rate = (L-ex_range)/L
-    i_points = points*i_amplify_rate
+    # p1 = points[0]
+    # p2 = points[1]
+    # L = np.abs(p1[0]*p2[1]-p1[1]*p2[0])/np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
-    o_cut_area,o_area = IntersectArea(TOWER,o_points)
+    
+
+    # tower = Polygon(TOWER).convex_hull
+
+    #o_cut_area,o_area = IntersectArea(TOWER,o_points)
     m_cut_area,m_area = IntersectArea(TOWER,points)
-    i_cut_area,i_area = IntersectArea(TOWER,i_points)
+    
 
-    # 被截断的面积(占自身面积)
-    o_cut_area-=m_cut_area
-    m_cut_area-=i_cut_area
-    # 自身总面积
-    o_area-=m_area
-    m_area-=i_area
+    # # 被截断的面积(占自身面积)
+    # o_cut_area-=m_cut_area
+    # # 自身总面积
+    # o_area-=m_area
 
-    total_energy = i_area + 0.75*m_area + 0.25*o_area
-    residu_energy = i_cut_area + 0.75*m_cut_area + 0.25*o_cut_area
+    total_energy = m_area
+    residu_energy = m_cut_area
 
     energy_rate = residu_energy/total_energy
 
--- IntersectArea.py
+++ IntersectArea.py
@@ -4,14 +4,14 @@ from shapely.geometry import Polygon
 输入两四边形在同一平面坐标系内的坐标
 返回重叠面积
 '''
-def IntersectArea(tower,mirror):
+def IntersectArea(t,mirror):
 
     # data1 = [[1,1],[2,1],[2,3],[1,3]]
 
     # data2 = [[1,2],[1.5,1],[3,4],[2,4]]
 
 
-    poly1 = Polygon(tower).convex_hull
+    poly1 = Polygon(t).convex_hull
     poly2 = Polygon(mirror).convex_hull
 
     if poly1.intersects(poly2):
@@ -19,4 +19,4 @@ def IntersectArea(tower,mirror):
     else:
         inter_area = 0
 
-    return inter_area,poly2.area
+    return [inter_area,poly2.area]
